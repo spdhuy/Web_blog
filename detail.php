4de72b66ee7b0309eabaf5ef5e5ceb1b68acb98e
Nhung <nguyennhungcl1505@gmail.com>
2017-07-14 17:57:00 +0700

    blog

---
+++ detail.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <div class="metabar-inner">
+        <div class="col-md-9 metabar-block-left">
+            <img src="img/lodo.png" alt="logo">
+        </div>
+        <div class="col-md-3 metabar-block-right">
+            <a href="#">Write a story</a>
+            <a href="#" style="color: black">Sign in/ Sign up</a>
+            <span class="glyphicon glyphicon-search"></span>
+        </div>
+    </div>
+    <div class="col-md-12 author">
+        <a href="#"><img src="img/thomas oppong icon.jpeg" height="60" width="60"/></a>
+        <div>
+            <a href="#">Thomas Oppong</br></a>
+            <a href="#"><input type="button" value="Follow"></a>
+            <p>npm CLI team’s green-haired digital dryad. Boricua. Queer af. @wealljs admin. http://pronoun.is/they?or=she</p>
+            <time datetime="2017-07-13">July 7 . 6 min read</time>
+        </div>
+    </div>
+
+
+</div>
+</body>
+</html>
---
+++ web_blog.php
@@ -0,0 +1,233 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 header">
+            <div class="metabar-inner">
+                <div class="col-md-9 metabar-block-left">
+                    <img src="img/lodo.png" alt="logo">
+                </div>
+                <div class="col-md-3 metabar-block-right">
+                    <a href="#">Write a story</a>
+                    <a href="#" style="color: black">Sign in/ Sign up</a>
+                    <span class="glyphicon glyphicon-search"></span>
+                </div>
+            </div>
+            <div class=" metabar-inner">
+                <div class=" metabar-block">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Popular on medium</a></li>
+                        <li><a href="#">Audio</a></li>
+                        <li><a href="#">Member only</a></li>
+                        <li><a href="#">Handpicked by Medium staff</a></li>
+                        <li><a href="#">Technology</a></li>
+                        <li><a href="#">Creativity</a></li>
+                        <li><a href="#">Entrepreneurship</a></li>
+                        <li><a href="#">Culture</a></li>
+                        <li><a href="#">Self</a></li>
+                        <li><a href="#">Politic</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 content">
+
+            <div class="flexCenter">
+                <a href="#"><h2>Popular on medium</h2></a>
+                <a href="#" class="flex">
+                    <h4>MORE <small><span class="glyphicon glyphicon-chevron-right"></span></small></h4>
+
+                </a>
+            </div>
+            <div>
+                <div class="col-md-6 content-item">
+                    <a href="detail.php">
+                        <img src="img/thomas oppong.jpeg" height="1334" width="2000"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 content-item">
+                    <a href="#">
+                        <img src="img/thomas oppong.jpeg" height="1334" width="2000"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 content-item">
+                    <a href="#">
+                        <img src="img/thomas oppong.jpeg" height="1334" width="2000"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 content-item">
+                    <a href="#">
+                        <img src="img/thomas oppong.jpeg" height="1334" width="2000"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-12 content">
+            <div class="flexCenter">
+                <a href="#"><h2>Audio</h2></a>
+                <a href="#" class="flex">
+                    <h4>MORE <small><span class="glyphicon glyphicon-chevron-right"></span></small></h4>
+
+                </a>
+            </div>
+            <div>
+                <div class="col-md-4 content-item">
+                    <a href="#"><img src="img/john boik.jpg"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <h4> <span class="glyphicon glyphicon-volume-down"></span> Audio available</h4>
+
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4 content-item">
+                    <a href="#"><img src="img/john boik.jpg"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <h4> <span class="glyphicon glyphicon-volume-down"></span> Audio available</h4>
+
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4 content-item">
+                    <a href="#"><img src="img/john boik.jpg"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <h4> <span class="glyphicon glyphicon-volume-down"></span> Audio available</h4>
+
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4 content-item">
+                    <a href="#"><img src="img/john boik.jpg"/></a>
+                    <div class="content-item-right">
+                        <div class="content-item-right-top">
+                            <h4> <span class="glyphicon glyphicon-volume-down"></span> Audio available</h4>
+
+                            <a href="#"> <h3>The Inconvenient Truths About Getting Work Done</h3></a>
+                            <a href="#"><h4>Humans are prolific procrastinators. People have struggled with habitual hesitation going back to ancient civilizations. It’s easy to make…</h4></a>
+                        </div>
+                        <div class="content-item-right-bottom">
+                            <a href="#"><img src="img/thomas oppong icon.jpeg" height="40" width="40"/></a>
+                            <a href="#"><span class="glyphicon glyphicon-heart-empty"></span></a>
+                            <a href="#">Thomas Oppong</br></a>
+                            <time datetime="2017-07-13">July 7 . 6 min read</time>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 fooder">
+            <div>
+                <ul>
+                    <li><a href="#">Getting started</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Status</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Terms</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
